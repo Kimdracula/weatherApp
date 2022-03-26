2d0d311bb5b916552265c5c3c9e9862b2b19f1e9
Tyler <vsnow150290@gmail.com>
2022-03-26 22:56:12 +0300

    Старт проекта

---
+++ app/src/main/java/com/homework/weatherapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homework.weatherapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.homework.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
