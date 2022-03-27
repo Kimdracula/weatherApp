d8e33cf2a50f3a4e91891e99369aaa91001319eb
Tyler <vsnow150290@gmail.com>
2022-03-27 14:50:26 +0300

    Проверил, всё работает, осталось внести мелкие правки

--- app/src/main/java/com/homework/weatherapp/model/Weather.kt
+++ app/src/main/java/com/homework/weatherapp/model/Weather.kt
@@ -1,6 +1,6 @@
 package com.homework.weatherapp.model
 
-data class Weather(val city:City =getDefaultCity(), val temperature: Int =0, val fellsLike :Int=0)
+data class Weather(val city:City =getDefaultCity(), val temperature: Int =-20, val fellsLike :Int=-30)
 
 data class City(val name:String,val lat:Double,val lon:Double)
 
--- app/src/main/java/com/homework/weatherapp/view/MainFragment.kt
+++ app/src/main/java/com/homework/weatherapp/view/MainFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import com.homework.weatherapp.R
 import com.homework.weatherapp.databinding.FragmentMainBinding
 import com.homework.weatherapp.view_model.MainViewModel
+import com.homework.weatherapp.view_model.ResponseState
 
 
 class MainFragment : Fragment() {
@@ -24,7 +27,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater,container,false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -38,14 +41,33 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val observer = Observer<Any> {
+        val observer = Observer<ResponseState> {
             renderData(it)
         }
         viewModel.getData().observe(viewLifecycleOwner, observer)
-
+        viewModel.getWeather()
     }
 
-    private fun renderData(it: Any?) {
-        Toast.makeText(requireContext(), "It's Alive!", Toast.LENGTH_LONG).show()
+    private fun renderData(data: ResponseState) {
+        when (data) {
+            is ResponseState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Toast.makeText(requireContext(), "It's Alive!", Toast.LENGTH_LONG).show()
+            }
+            is ResponseState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is ResponseState.Success->{
+                binding.loadingLayout.visibility = View.GONE
+                binding.cityName.text = data.weatherData.city.name.toString()
+                binding.coordinates.text = "Широта: ${data.weatherData.city.lat} Долгота: ${data.weatherData.city.lon}"
+                binding.temperature.text = data.weatherData.temperature.toString()
+                binding.feelsLike.text = data.weatherData.fellsLike.toString()
+
+
+            }
+        }
+
+
     }
 }
