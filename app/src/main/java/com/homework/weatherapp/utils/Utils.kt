1648da51a50690f9345dd13dba1300c60f685add
Vlad <vlad-ard@ya.ru>
2022-04-25 12:07:38 +0300

    Исправил ошибку отображения картинки погоды, давления, состояния

--- app/src/main/java/com/homework/weatherapp/utils/Utils.kt
+++ app/src/main/java/com/homework/weatherapp/utils/Utils.kt
@@ -6,21 +6,17 @@ import com.homework.weatherapp.model.WeatherDTO
 import com.homework.weatherapp.model.getDefaultCity
 
 const val KEY_BUNDLE_WEATHER = "key"
-const val KEY_INTENT_LAT = "keyLat"
-const val KEY_INTENT_LON = "keyLon"
-const val KEY_BROADCAST_INTENT = "keyBroadcastIntent"
-const val KEY_BROADCAST_MESSAGE = "keyBroadcastMessage"
 const val SCHEME = "https:"
 const val AUTHORITY = "api.weather.yandex.ru"
 const val END_POINT = "v2/informers?"
 const val QUERY_LAT = "lat"
 const val QUERY_LON = "lon"
 const val WEATHER_API_LOGIN = "X-Yandex-API-Key"
-const val KEY_BROADCAST_ERROR_MESSAGE = "keyBroadcastErrorMessage"
+
 
 
 fun convertDtoToModel(weatherDTO: WeatherDTO): Weather {
     val fact: FactDTO = weatherDTO.factDTO
-    return (Weather(getDefaultCity(), fact.temp, fact.feelsLike,fact.icon))
+    return (Weather(getDefaultCity(), fact.temp, fact.feelsLike, fact.condition, fact.humidity,fact.icon))
 }
 
