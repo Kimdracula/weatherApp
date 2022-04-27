f20c64ae29e0190f5208e8eac0767fd27c4e33d1
Vlad <vlad-ard@ya.ru>
2022-04-27 16:08:31 +0300

    БД готова

---
+++ app/src/main/java/com/homework/weatherapp/App.kt
@@ -0,0 +1,34 @@
+package com.homework.weatherapp
+
+import android.app.Application
+import androidx.room.Room
+import com.homework.weatherapp.domain.room.HistoryDAO
+import com.homework.weatherapp.domain.room.HistoryDB
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+
+
+    companion object {
+        private var appContext: App? = null
+        private var db: HistoryDB? = null
+        fun getHistoryDAO(): HistoryDAO {
+            if (db == null) {
+                db = appContext?.let {
+                    Room.databaseBuilder(
+                        it,
+                        HistoryDB::class.java, "database-history"
+                    )
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+return db!!.historyDAO()
+
+        }
+    }
+}
--- app/src/main/java/com/homework/weatherapp/domain/room/HistoryDB.kt
+++ app/src/main/java/com/homework/weatherapp/domain/room/HistoryDB.kt
@@ -4,6 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [HistoryEntity::class], version = 1)
-abstract class Database:RoomDatabase() {
+abstract class HistoryDB:RoomDatabase() {
     abstract fun historyDAO():HistoryDAO
 }
